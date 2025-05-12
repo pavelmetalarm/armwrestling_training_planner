7b5d4f2707d722d1728638e1e19bd2f4e0281fa9
Paul Novikov <49231300+pavelmetalarm@users.noreply.github.com>
2025-05-12 12:18:13 +0300

    Ci/fix prod deploy (#3)
    ci(fix-prod-deploy): upd deploy_prod workflow
    
    - add raw bash into "scripts"
    - add several separated steps
    - add final systemd check after service deployment(restart)
    
    Won't remove .deploy/ now.

---
+++ .deploy/env_vars_prod.sh
@@ -0,0 +1,7 @@
+# Deployment configuration
+
+BRANCH="main"
+USER="armwrestling_training_bot"
+PORT="8080"
+WORKDIR="/home/armwrestling_training_bot/armwrestling_training_planner"
+PYTHONPATH="$WORKDIR/.venv/bin/python"
---
+++ .deploy/preparations.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# 1. Check Python version
+echo "[Check] Python version:"
+python3 --version || { echo " Python3 not found"; exit 1; }
+
+# 2. Check if pip is available and install (upgrade) it
+echo "[Check] Checking for pip..."
+
+if ! python3 -m pip --version >/dev/null 2>&1; then
+    echo "[Missing] pip3 not found, installing it"
+    apt-get install python3-pip
+else
+    echo "[OK] pip is present, upgrading..."
+    python3 -m pip install --upgrade pip
+fi
+
+# 3. Check if requirements.txt exists
+if [ ! -f "../requirements/base.txt" ]; then
+    echo " ../requirements/base.txt not found"
+    exit 1
+fi
+echo "[Check] Found requirements.txt"
+
+# 4. Create virtual environment if missing
+if [ ! -d "~/.venv" ]; then
+    echo "[Action] Creating virtual environment..."
+    python3 -m venv ~/.venv
+else
+    echo "[Skip] Virtual environment already exists"
+fi
+
+# 5. Activate virtual environment
+echo "[Action] Activating virtual environment..."
+source ~/.venv/bin/activate
+
+# 6. Install dependencies
+echo "[Action] Installing dependencies..."
+pip install -r ../requirements/base.txt
+
+# 5. Deactivate virtual environment
+echo "[Action] Deactivating virtual environment..."
+deactivate
